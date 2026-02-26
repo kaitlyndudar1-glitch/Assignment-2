3517ae280bc3ed3e94fdd37bd3cc42f00e03f48a
kaitlyndudar1-glitch <KaitlynDudar1@gmail.com>
2026-02-26 16:47:35 -0600

    Word Reader fixed file

--- DudarKaitlynA2Q1.py
+++ DudarKaitlynA2Q1.py
@@ -1,1 +1,14 @@
-print("Hello World")
+"""DudarKaitlynA2Q1
+COMP 1012 SECTION A01
+INSTRUCTOR Dr. Saulo Q. Dos Santos
+ASSIGNMENT: A2 Question 1
+AUTHOR Kaitlyn Dudar
+VERSION [Date of last change; e.g., 2026-Feb-26]
+PURPOSE: use of dictionaries to store and analyze word frequencies of two files
+"""
+
+def readWords(userWords):
+    fileWords= open(userWords)
+    wordCount= fileWords.read().split()
+    fileWords.close()
+    return wordCount
