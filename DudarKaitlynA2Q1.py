a724b101914367e07f92809792ccc7c9fc4bbe4b
kaitlyndudar1-glitch <KaitlynDudar1@gmail.com>
2026-02-27 16:30:42 -0600

    date fix

--- DudarKaitlynA2Q1.py
+++ DudarKaitlynA2Q1.py
@@ -3,7 +3,7 @@ COMP 1012 SECTION A01
 INSTRUCTOR Dr. Saulo Q. Dos Santos
 ASSIGNMENT: A2 Question 1
 AUTHOR Kaitlyn Dudar
-VERSION [Date of last change; e.g., 2026-Feb-26]
+VERSION [Date of last change; e.g., 2026-Feb-27]
 PURPOSE: use of dictionaries to store and analyze word frequencies of two files
 """
 def readWords(userWords):
--- DudarKaitlynA2Q2.py
+++ DudarKaitlynA2Q2.py
@@ -3,7 +3,7 @@ COMP 1012 SECTION A01
 INSTRUCTOR Dr. Saulo Q. Dos Santos
 ASSIGNMENT: A2 Question 2
 AUTHOR Kaitlyn Dudar
-VERSION [Date of last change; e.g., 2026-Feb-26]
+VERSION [Date of last change; e.g., 2026-Feb-27]
 PURPOSE: calculates floor occupancy for flexible parking pricing
 """
 '''
