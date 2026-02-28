bc4cbdebf848ae0240fd2eee9a413ee9794a5d16
kaitlyndudar1-glitch <KaitlynDudar1@gmail.com>
2026-02-27 19:12:20 -0600

    Final final

--- DudarKaitlynA2Q3.py
+++ DudarKaitlynA2Q3.py
@@ -3,7 +3,7 @@ COMP 1012 SECTION A01
 INSTRUCTOR Dr. Saulo Q. Dos Santos
 ASSIGNMENT: A2 Question 3
 AUTHOR Kaitlyn Dudar
-VERSION [Date of last change; e.g., 2026-Feb-22]
+VERSION [Date of last change; e.g., 2026-Feb-27]
 PURPOSE: Analyze historical Bitcoin price data to calculate various statistics
 """
 import math
